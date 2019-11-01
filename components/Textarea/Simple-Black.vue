ad387e55aead0236ab3127cddafd6bb91efe45cb
iftakhar ahmed <ifat802@gmail.com>
2019-11-01 12:27:17 +0600

    :art:Simple textarea

---
+++ components/Textarea/Simple-Black.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="textarea">
+        <p class="order__text">{{$t('Your message here:')}}</p>
+        <textarea
+            v-model="formData.message"
+            class="textarea--area"
+            name="message"
+            id
+            rows="10"
+        ></textarea>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="scss" scoped>
+.textarea {
+    &--area {
+      width: 100%;
+      resize: none;
+      border: 1px solid $color-black;
+      padding: 1rem;
+      font-size: 16px;
+      font-family: $font-1;
+      &:focus {
+        outline: none;
+      }
+    }
+  }
+</style>
